81ae75b7eea63855c36ebb92ce3459dec4e58769
Tristan Weil <titou@lab.t18s.fr>
2020-11-12 23:12:02 +0100

    fix: force the update of the alpine docker image

--- docker-build.sh
+++ docker-build.sh
@@ -20,7 +20,7 @@ echo "Building Artifact..."
 CGO_ENABLED=0 ${GO_BUILD_CMD} "${GO_BUILD_OPT}" -o "${BUILD_DIR}/ttserver"
 
 echo "Building Docker..."
-docker build -t latest .
+docker build --pull -t latest .
 
 echo "Starting Docker..."
 docker run -it --rm --name ttserver -p 7070:7272 -e TTSERVER_LISTENER_ADDRESS=0.0.0.0:7272 latest
